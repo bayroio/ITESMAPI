c971a0213f9e2e18dcbf077ca77286c8f95df9c5
Omar <mandragom@gmail.com>
2022-11-24 23:52:14 -0600

    Add IPFS

--- src/routes/step_v1.ts
+++ src/routes/step_v1.ts
@@ -1,7 +1,7 @@
 import { Request, Response, Router } from "express";
 import multer from 'multer';
-//import FileReader from FileReader;
 import Web3 from "web3";
+import * as IPFS from 'ipfs-core';
 import "dotenv/config";
 import { keccak_256 } from 'js-sha3';
 import { LSPFactory } from "@lukso/lsp-factory.js";
@@ -26,53 +26,63 @@ router.post('/', upload.single("file"), async (req: Request, res: Response) => {
     var hash = keccak_256(req.file?.buffer || "");
     console.log(hash);
 
-    //Create the Address
-    const web3 = new Web3();
-    const myEOA = web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY || "");
+    //load file in ipfs
+    const filetemp = {
+      path: `tmp/${req.file?.originalname}`,
+      content: req.file?.buffer
+    }
+    const ipfs = await IPFS.create();
+    const result = await ipfs.add(filetemp);
+    console.info(result)
 
-    //Connect to ganache
-    const lspFactory = new LSPFactory(RPC_GANACHE, {
-        deployKey: process.env.PRIVATE_KEY, // Private key of the account which will deploy any smart contract,
-        chainId: 5777,
-    });
+
+    // //Create the Address
+    // const web3 = new Web3();
+    // const myEOA = web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY || "");
+
+    // //Connect to ganache
+    // const lspFactory = new LSPFactory(RPC_GANACHE, {
+    //     deployKey: process.env.PRIVATE_KEY, // Private key of the account which will deploy any smart contract,
+    //     chainId: 5777,
+    // });
  
-    //Create the MetaData
-    const myLSP3MetaData = {
-        name: "Test",
-        description: "Personal Data",
-        correo: req.body.correo,
-        nombre: req.body.nombre,
-        primerapellido: req.body.primerapellido,
-        segundoapellido: req.body.segundoapellido
-    };
+    // //Create the MetaData
+    // const myLSP3MetaData = {
+    //     name: "Test",
+    //     description: "Personal Data",
+    //     correo: req.body.correo,
+    //     nombre: req.body.nombre,
+    //     primerapellido: req.body.primerapellido,
+    //     segundoapellido: req.body.segundoapellido
+    // };
 
 
-    //Create the smart contract
-    const profileDeploymentEvents = [];
-    const address = process.env.ADDRESS || "";
-    const myContracts = await lspFactory.UniversalProfile.deploy({
-        controllerAddresses: [myEOA.address], // Account addresses which will control the UP
-        lsp3Profile: myLSP3MetaData,
-    },
-    {
-        onDeployEvents: {
-           next: (deploymentEvent) => {
-                console.log(deploymentEvent);
-                profileDeploymentEvents.push(deploymentEvent);
-            },
-            error: (error) => {
-                console.error(error);
-                res.send("Error");
-            },
-            complete: (contracts) => {
-                console.log('Universal Profile deployment completed');
-                console.log("Mi UP Address", contracts.LSP0ERC725Account?.address);
-                console.log(contracts);
-                console.log("-----------------------------------------------");
-                res.send(`Mi UP Address ${contracts.LSP0ERC725Account?.address}`)
-            },
-        }
-    });
+    // //Create the smart contract
+    // const profileDeploymentEvents = [];
+    // const address = process.env.ADDRESS || "";
+    // const myContracts = await lspFactory.UniversalProfile.deploy({
+    //     controllerAddresses: [myEOA.address], // Account addresses which will control the UP
+    //     lsp3Profile: myLSP3MetaData,
+    // },
+    // {
+    //     onDeployEvents: {
+    //        next: (deploymentEvent) => {
+    //             console.log(deploymentEvent);
+    //             profileDeploymentEvents.push(deploymentEvent);
+    //         },
+    //         error: (error) => {
+    //             console.error(error);
+    //             res.send("Error");
+    //         },
+    //         complete: (contracts) => {
+    //             console.log('Universal Profile deployment completed');
+    //             console.log("Mi UP Address", contracts.LSP0ERC725Account?.address);
+    //             console.log(contracts);
+    //             console.log("-----------------------------------------------");
+    //             res.send(`Mi UP Address ${contracts.LSP0ERC725Account?.address}`)
+    //         },
+    //     }
+    // });
 
     res.send("hola");
 })
