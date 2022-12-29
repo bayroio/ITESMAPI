fa63e9f4ab9c474e6980060339d9a993a2d1bfff
Omar <mandragom@gmail.com>
2022-12-28 20:11:09 -0600

    Fix GetUniversalProfile

--- src/routes/GetUniversalProfile_v1.ts
+++ src/routes/GetUniversalProfile_v1.ts
@@ -1,41 +1,57 @@
 import { Request, Response, Router } from "express";
 import { ERC725 } from '@erc725/erc725.js';
-import { RPC_GANACHE, IPFS_GATEWAY } from "./constants";
-import 'isomorphic-fetch';
+import { RPC_GANACHE, PORT_GANACHE, RPC_ENDPOINT_L16, PORT_ENDPOINT_L16, IPFS_GATEWAY } from "./constants";
 import erc725schema from '@erc725/erc725.js/schemas/LSP3UniversalProfileMetadata.json';
 import Web3 from "web3";
+
+
 const router = Router();
 
-// Parameters for ERC725 Instance
-const provider = new Web3.providers.HttpProvider(RPC_GANACHE);
-const config = { ipfsGateway: IPFS_GATEWAY };
 
-async function fetchProfile(address: any) {
-    try {
-      const profile = new ERC725(erc725schema, address, provider, config);
-      return await profile.fetchData();
-    } catch (error) {
-      return console.log('This is not an ERC725 Contract');
-    }
-  }
-  
 router.post('/', async (req: Request, res: Response) => {
-    const profileData = await fetchProfile(process.env.ADDRESS);
-    let result = JSON.parse(JSON.stringify(profileData));
+  try {
+    let endpoint = "";
+    let port = 0;
 
-    //General
-    console.log("Name: " + result[1].value.LSP3Profile.name);
-    console.log("Description: " + result[1].value.LSP3Profile.description);
-    console.log("Correo: " + result[1].value.LSP3Profile.correo);
-    console.log("Nombre: " + result[1].value.LSP3Profile.nombre);
-    console.log("Primer Apellido: " + result[1].value.LSP3Profile.primerapellido);
-    console.log("Segundo Apellido: " + result[1].value.LSP3Profile.segundoapellido);
+    //Get the general info 
+    const UniversalProfile = process.env.UNIVERSALPROFILE || "";
+    if (process.env.Endpoint == 'GANACHE') {
+      endpoint = RPC_GANACHE;
+      port = PORT_GANACHE;
+    }
+    else if (process.env.Endpoint == 'LUKSO_L16') {
+      endpoint = RPC_ENDPOINT_L16;
+      port = PORT_ENDPOINT_L16;
+    }
+
+    // Parameters for ERC725 Instance
+    const provider = new Web3.providers.HttpProvider(endpoint);
+    const config = { ipfsGateway: IPFS_GATEWAY };
     
-    //Image
-    console.log("Background image: " + IPFS_GATEWAY + "/" + result[1].value.LSP3Profile.backgroundImage[0].url);
-    console.log("Profile image: " + IPFS_GATEWAY + "/" + result[1].value.LSP3Profile.profileImage[0].url);
 
-    res.send("Complete");
+    const profile = new ERC725(erc725schema, UniversalProfile, provider, config);
+    const result = JSON.parse(JSON.stringify(await profile.fetchData()));
+    console.log(result);
+    res.send(result);
+
+    // let result = JSON.parse(JSON.stringify(profileData));
+
+    // console.log(result);
+    // //General
+    // console.log("Name: " + result[1].value.LSP3Profile.name);
+    // console.log("Description: " + result[1].value.LSP3Profile.description);
+    // console.log("Correo: " + result[1].value.LSP3Profile.correo);
+    // console.log("Nombre: " + result[1].value.LSP3Profile.nombre);
+    // console.log("Primer Apellido: " + result[1].value.LSP3Profile.primerapellido);
+    // console.log("Segundo Apellido: " + result[1].value.LSP3Profile.segundoapellido);
+    
+    // //Image
+    // console.log("Background image: " + IPFS_GATEWAY + "/" + result[1].value.LSP3Profile.backgroundImage[0].url);
+    // console.log("Profile image: " + IPFS_GATEWAY + "/" + result[1].value.LSP3Profile.profileImage[0].url);
+  } 
+  catch (error) {
+    return console.log('This is not an ERC725 Contract');
+  }
 })
 
 
